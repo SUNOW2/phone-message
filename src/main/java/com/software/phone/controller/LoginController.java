4e7e896995da7ef2956521e22ca7cf20e271c796
徐旭 <1605611836@qq.com>
2018-06-07 10:25:22 +0800

    添加日志

--- src/main/java/com/software/phone/controller/LoginController.java
+++ src/main/java/com/software/phone/controller/LoginController.java
@@ -3,11 +3,13 @@ package com.software.phone.controller;
 import com.software.phone.conf.ResponseEntity;
 import com.software.phone.po.LoginPo;
 import com.software.phone.service.LoginService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/phone")
 public class LoginController extends BaseController {
@@ -22,6 +24,7 @@ public class LoginController extends BaseController {
      */
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseEntity login(LoginPo loginPo) {
+        log.info("用户获取验证码");
         if(loginService.sendMessage(loginPo).split(":")[1].split(",")[0].equals("1")) {
             return this.getSuccessResult();
         }
@@ -30,6 +33,7 @@ public class LoginController extends BaseController {
 
     @RequestMapping(value = "/oauth", method = RequestMethod.POST)
     public ResponseEntity oauth(LoginPo loginPo) {
+        log.info("用户携带验证码进行登录验证");
         if(loginService.oauth(loginPo)) {
             return this.getSuccessResult();
         }
